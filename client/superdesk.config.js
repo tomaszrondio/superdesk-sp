ffe9dd29e532618dcda16f04a90834ae0611999b
Tomasz Rondio <tomasz.rondio@gmail.com>
2021-09-14 14:16:24 +0200

    WIP

--- client/superdesk.config.js
+++ client/superdesk.config.js
@@ -10,6 +10,7 @@ module.exports = function(grunt) {
             'superdesk-publisher'
         ],
         importApps: [
+            '../index',
             'superdesk-publisher'
         ],
         defaultRoute: '/workspace/personal',
